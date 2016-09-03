285e777b4a9d8aeb6b6328b05596c2def6dedbed
Pixelpille <martin@brunnert.me>
2016-09-03 18:43:45 +0200

    Add files via upload

---
+++ dash.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python2 
+
+from scapy.all import *
+import os, time
+
+def arp_display(pkt):
+  if pkt.haslayer(ARP):        
+        # Hallway
+        if pkt[ARP].hwsrc == '38:f2:3e:e4:aa:9c':
+          print "Hallway"
+	  os.system("mpc toggle")
+      
+        # Kitchen
+        elif pkt[ARP].hwsrc == '5c:35:3b:de:ad:07':
+          print "Kitchen"
+	  os.system("mpc toggle")
+
+	# Couch
+        elif pkt[ARP].hwsrc == 'dc:53:7c:ae:5c:78':
+	   print "Couch"
+	   os.system("mpc toggle")
+	    
+	# Bed
+        elif pkt[ARP].hwsrc == 'dc:53:7c:b5:37:93':
+	  print "Bed"
+	  os.system("mpc toggle")
+	  
+print sniff(prn=arp_display, filter="arp", count=1,  store=0)
---
+++ listen.py
@@ -0,0 +1,7 @@
+from scapy.all import *
+
+def arp_display(pkt):
+  if pkt[ARP].op == 1: #who-has (request)
+      print "ARP Probe from: " + pkt[ARP].hwsrc
+
+print sniff(prn=arp_display, filter="arp", store=0)
